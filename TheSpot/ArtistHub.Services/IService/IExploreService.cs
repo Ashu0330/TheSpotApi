f0ae1f36bcad30ce4d572921829a9b8be82f444b
Ashu0330 <himanshu.suman0076@gmail.com>
2026-04-22 10:05:47 +0530

    Fixed gitignore tracking

--- TheSpot/ArtistHub.Services/IService/IExploreService.cs
+++ TheSpot/ArtistHub.Services/IService/IExploreService.cs
@@ -11,7 +11,7 @@ namespace ArtistHub.Services.IService
     public interface IExploreService
     {
         ValueTask<ApiResponse<TblArtist>> GetArtistByUserId(long userId);
-        ValueTask<ApiResponse<IEnumerable<TblArtist>>> GetAllArtist();
+        ValueTask<ApiResponse<IEnumerable<object>>> GetAllArtist();
         ValueTask<ApiResponse<IEnumerable<ArtistDto>>> GetArtistByCategory(ArtistExploreFilterModel model);
         ValueTask<ApiResponse<IEnumerable<LoungeDto>>> GetLoungeByCategory(LoungeExploreFilterModel model);
         ValueTask<ApiResponse<IEnumerable<EventDto>>> GetEventByCategory(EventExploreFilterModel model);
